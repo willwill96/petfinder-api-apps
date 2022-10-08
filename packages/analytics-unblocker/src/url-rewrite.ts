c28ad993bc878d225111461330b9efaffd087bfb
William Will <10997562+willwill96@users.noreply.github.com>
2022-10-08 21:26:29 +0000

    fixes

--- packages/analytics-unblocker/src/url-rewrite.ts
+++ packages/analytics-unblocker/src/url-rewrite.ts
@@ -1,5 +1,6 @@
 const domains = [
   'doubleclick.net',
+  'stats.g.doublick.net',
   'tagassistant.google.com',
   'googletagmanager.com',
   'analytics.google.com',
@@ -8,8 +9,6 @@ const domains = [
 
 const urlRewrite = (input: string): string => {
   const regexMatch = input.match(/(https?:\/\/)([^/?]*)(\/|\?)?(.*)/)
-  console.log('input', input)
-  console.log('regexMatch', regexMatch)
   if (regexMatch && domains.includes(regexMatch[2])) {
     return `${location.protocol}//${
       location.host
