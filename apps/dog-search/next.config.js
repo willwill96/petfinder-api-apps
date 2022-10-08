c5c6826e0d7a28c51de2d0567d1b01e1a2ad733a
William Will <10997562+willwill96@users.noreply.github.com>
2022-10-08 23:22:24 +0000

    use rewrites & index page instead of dynamic route

--- apps/dog-search/next.config.js
+++ apps/dog-search/next.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   reactStrictMode: true,
   pageExtensions: ['tsx', 'ts'],
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/analytics-proxy/(.*)',
+        destination: '/api/analytics-proxy',
+      },
+    ]
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.analytics-unblocker\/index\.js$/i,
